1b67dee75ae8c47eebf6d49c84bdbca7c64496e6
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-06-15 21:18:48 -0500

    Add imports to app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+import openai
+import json
+import os
+from dotenv import load_dotenv
+from pyairtable import Table
+import requests
+
+
+
 user_query = """ 
 What's a stock that has the biggest price movement today?
 Summarize the latest news to analyse potential causes & add to it to my Airtable database/
