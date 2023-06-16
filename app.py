f3965472d4e8f590466edc5d3b554ec17f642276
Hoard Coin <119143763+tanvirkhon@users.noreply.github.com>
2023-06-15 21:14:58 -0500

    Sudo code for project

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ What's a stock that has the biggest price movement today?
 Summarize the latest news to analyse potential causes & add to it to my Airtable database/
 """
 
+# get_stock_movers
+
+
+# get_stock_news
+
+
+# add_stock_news_airtable
